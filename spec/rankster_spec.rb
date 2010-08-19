4941ea75795a0410c700c7894ee60648387cd483
David Padilla <david@crowdint.com>
2010-08-18 18:07:43 -0700

    Tests with mocked responses

--- spec/rankster_spec.rb
+++ spec/rankster_spec.rb
@@ -1,5 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+# Mock response object
+class HTTPResponse
+  attr_accessor :body
+  def initialize(body)
+    self.body = body
+  end
+end
+
 describe Rankstar do
   describe :rank do
     it "raises an exception if you use an unsupported engine" do
@@ -14,8 +22,22 @@ describe Rankstar do
       lambda {Rankstar.rank(:google, :keyword => 'keyword')}.should raise_error
     end
 
-    it "returns crowdint as the #1 site on google for www.crowdint.com" do
+    it "returns crowdint with a rank of 1 on google for www.crowdint.com" do
+      # Mock Google's response
+      Net::HTTP.should_receive(:get_response).and_return(HTTPResponse.new(google_response))
       Rankstar.rank(:google, :keyword => 'crowdint', :url => 'www.crowdint.com', :limit => 300).should == 1  
+    end
+
+    it "returns crowdint with a rank of 2 on yahoo for www.crowdint.com" do
+      # Mock Yahoo's response
+      Net::HTTP.should_receive(:get_response).and_return(HTTPResponse.new(yahoo_response))
+      Rankstar.rank(:yahoo, :keyword => 'crowdint', :url => 'www.crowdint.com', :limit => 300).should == 2
+    end
+
+    it "returns crowdint with a rank of 2 on bing for www.crowdint.com" do
+      # Mock Bing's response
+      Net::HTTP.should_receive(:get_response).and_return(HTTPResponse.new(bing_response))
+      Rankstar.rank(:bing, :keyword => 'crowdint', :url => 'www.crowdint.com', :limit => 300).should == 2
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
 require 'rankstar'
 require 'spec'
 require 'spec/autorun'
